7d730994399f67666401f7e917be37757b7b1226
Enzo-Faria <enzofariamotadelima@gmail.com>
2026-03-04 23:46:30 -0300

    Add files via upload

---
+++ L1SO_15.py
@@ -0,0 +1,8 @@
+#Declarar
+C1 : float
+C2 : float
+#Início
+C1 = float(input("Digite o valor do primeiro cateto:"))
+C2 = float(input("Digite o valor do segundo cateto:"))
+print("O valor da Hipotenusa é:",(C1 ** 2 + C2 ** 2) ** 0.5)
+#Fim.
---
+++ L1SO_9.py
@@ -0,0 +1,8 @@
+#Declarar
+num1 : int
+num2 : int
+#Início
+num1 = int(input("Digite o primeiro número:"))
+num2 = int(input("Digite o segundo número:"))
+print("A soma dos quadrados dos números fornecidos é:",num1 ** 2 + num2 ** 2)
+#Fim.
