6acb2defe740d4f31a57165267bd74a6818abfbd
Enzo-Faria <enzofariamotadelima@gmail.com>
2026-03-04 23:44:43 -0300

    Delete L1SO_14.py

--- L1SO_14.py
+++
@@ -1,8 +0,0 @@
-#Declarar
-A1 : float
-A2 : float
-#Início
-A1 = float(input("Digite o valor do primeiro ângulo:"))
-A2 = float(input("Digite o valor do segundo ângulo:"))
-print("O valor do terceiro ângulo é:",180 - A1 - A2)
-#Fim
