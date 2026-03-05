8b187ef352101b4777e6bf90d78708c08bc1757e
Enzo-Faria <enzofariamotadelima@gmail.com>
2026-03-04 23:40:25 -0300

    Add files via upload

--- L1SO_16.py
+++ L1SO_16.py
@@ -8,5 +8,5 @@ H = float(input("Digite as horas trabalhadas:"))
 V = float(input("Digite o valor de uma hora de trabalho:"))
 PD = float(input("Digite o valor de desconto:"))
 D = float(input("Digite o número de dependentes:"))
-print(("O salário a receber é:",H * V + 100 * D) * (1 - PD / 100))
+print("O salário a receber é:",(H * V + 100 * D) * (1 - PD / 100))
 #Fim.
